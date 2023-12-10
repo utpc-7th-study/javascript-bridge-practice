b53e28deccfbe586797d0d5c2df5a398a089afab
BK <3031011@naver.com>
2023-12-10 15:55:56 +0900

    feat: 이동 커맨드 입력받는 기능

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import BridgeGame from './BridgeGame.js';
-import { BRIDGE_LENGTH } from './constant/BridgeGame.js';
+import { BRIDGE_LENGTH, COMMAND } from './constant/BridgeGame.js';
 import ERROR_MESSAGE from './constant/BridgeGameMessage.js';
 import InputView from './view/InputView.js';
 import OutputView from './view/OutputView.js';
@@ -8,6 +8,7 @@ class App {
   async play() {
     const bridgeSize = await this.readBridgeSize();
     const bridgeGame = new BridgeGame(bridgeSize);
+    await this.readMoving();
   }
 
   async readBridgeSize() {
@@ -21,12 +22,29 @@ class App {
     }
   }
 
+  async readMoving() {
+    try {
+      const movingCommandInput = await InputView.readMoving();
+      this.#validateMovingCommand(movingCommandInput);
+      return movingCommandInput;
+    } catch (error) {
+      OutputView.print(error.message);
+      return this.readMoving();
+    }
+  }
+
   #validateBridgeSize(bridgeSizeInput) {
     const isValidSize =
       bridgeSizeInput >= BRIDGE_LENGTH.MIN && bridgeSizeInput <= BRIDGE_LENGTH.MAX;
 
     if (!isValidSize) {
       throw new Error(ERROR_MESSAGE.INVALID_BRIDGE_SIZE);
+    }
+  }
+
+  #validateMovingCommand(movingCommandInput) {
+    if (!(movingCommandInput === COMMAND.UP || movingCommandInput === COMMAND.DOWN)) {
+      throw new Error(ERROR_MESSAGE.INVALID_VALUE);
     }
   }
 }
