900d04a11e95991064efa4b538e77df7044981b4
BK <3031011@naver.com>
2023-12-10 14:16:38 +0900

    fix: InputView async-await 비동기 로직 추가

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -2,30 +2,30 @@ import { Console } from '@woowacourse/mission-utils';
 import { COMMAND } from '../constant/BridgeGame';
 
 const InputView = {
-  readBridgeSize() {
-    const bridgeSizeInput = this.read('다리의 길이를 입력해주세요.');
+  async readBridgeSize() {
+    const bridgeSizeInput = await this.read('다리의 길이를 입력해주세요.');
 
     return bridgeSizeInput;
   },
 
-  readMoving() {
-    const movingInput = this.read(
+  async readMoving() {
+    const movingInput = await this.read(
       `이동할 칸을 선택해주세요. (위: ${COMMAND.UP}, 아래: ${COMMAND.DOWN})`,
     );
 
     return movingInput;
   },
 
-  readGameCommand() {
-    const gameCommandInput = this.read(
+  async readGameCommand() {
+    const gameCommandInput = await this.read(
       `게임을 다시 시작할지 여부를 입력해주세요. (재시도: ${COMMAND.RESTART}, 종료: ${COMMAND.QUIT})`,
     );
 
     return gameCommandInput;
   },
 
-  read(message) {
-    const input = Console.readLineAsync(message);
+  async read(message) {
+    const input = await Console.readLineAsync(message);
 
     return input;
   },
