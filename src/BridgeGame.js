63c447ffd02978031f8f808d74636640d4cd8f0c
BK <3031011@naver.com>
2023-12-10 15:58:45 +0900

    feat: BridgeGame 클래스 구현
    이동 기능
    재시작 기능
    현재 상태 반환
    종료 여부 계산

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -16,32 +16,44 @@ class BridgeGame {
   #tryCount = 0;
 
   constructor(size) {
-    this.#assignBridge(BridgeMaker.makeBridge(size, BridgeRandomNumberGenerator.generate));
+    const bridge = this.#convertBridge(
+      BridgeMaker.makeBridge(size, BridgeRandomNumberGenerator.generate),
+    );
+    this.#bridge = bridge;
   }
 
-  /**
-   * 사용자가 칸을 이동할 때 사용하는 메서드
-   * <p>
-   * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
-  async move(command) {}
+  status() {
+    return {
+      U: this.#bridge.U.slice(0, this.#move),
+      D: this.#bridge.D.slice(0, this.#move),
+    };
+  }
 
-  /**
-   * 사용자가 게임을 다시 시도할 때 사용하는 메서드
-   * <p>
-   * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
-  retry() {}
+  done() {
+    return this.#bridge.U.length === this.#move;
+  }
 
-  #assignBridge(bridge) {
+  move(command) {
+    this.#bridge[command === COMMAND.UP ? 'D' : 'U'][this.#move] = null;
+    this.#move += 1;
+
+    return this.#bridge[command][this.#move];
+  }
+
+  retry() {
+    this.#move = 0;
+    this.#tryCount += 1;
+  }
+
+  #convertBridge(bridge) {
     bridge.forEach((crossAbleSection) => {
       if (crossAbleSection === COMMAND.UP) {
-        this.#bridge.U.push('O');
-        this.#bridge.D.push('X');
+        this.#bridge.U.push(true);
+        this.#bridge.D.push(false);
       }
       if (crossAbleSection === COMMAND.DOWN) {
-        this.#bridge.U.push('X');
-        this.#bridge.D.push('O');
+        this.#bridge.U.push(false);
+        this.#bridge.D.push(true);
       }
     });
   }
