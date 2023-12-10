9dd749836e0da6975754f1c124b595ce5915097a
sujinjwa <91577550+sujinjwa@users.noreply.github.com>
2023-12-10 17:23:27 +0900

    feat: 다리 길이 입력값이 숫자 아닌 경우 예외처리

--- src/validator/validator.js
+++ src/validator/validator.js
@@ -1,4 +1,10 @@
 const Validator = {
+  numberType(bridgeSize) {
+    if (isNaN(Number(bridgeSize))) {
+      throw new Error('[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.');
+    }
+  },
+
   invalidSize(bridgeSize) {
     if (bridgeSize < 3 || bridgeSize > 20) {
       throw new Error('[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.');
@@ -7,5 +13,6 @@ const Validator = {
 };
 
 export const validateBridgeSize = (bridgeSize) => {
+  Validator.numberType(bridgeSize);
   Validator.invalidSize(bridgeSize);
 };
