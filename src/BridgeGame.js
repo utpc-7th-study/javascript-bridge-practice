79d4496469f52dbc59b54142849f6f53be4de59d
BK <3031011@naver.com>
2023-12-10 15:01:26 +0900

    feat: 다리 건너기 게임 클래스에 입력받은 사이즈에 맞는 다리 생성하는 기능

--- src/BridgeGame.js
+++ src/BridgeGame.js
@@ -1,18 +1,22 @@
 import BridgeMaker from './BridgeMaker.js';
 import BridgeRandomNumberGenerator from './BridgeRandomNumberGenerator.js';
+import { COMMAND } from './constant/BridgeGame.js';
 
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
 class BridgeGame {
-  #bridge = [];
+  #bridge = {
+    U: [],
+    D: [],
+  };
 
   #move = 0;
 
   #tryCount = 0;
 
   constructor(size) {
-    this.#bridge = BridgeMaker.makeBridge(size, BridgeRandomNumberGenerator.generate);
+    this.#assignBridge(BridgeMaker.makeBridge(size, BridgeRandomNumberGenerator.generate));
   }
 
   /**
@@ -20,7 +24,7 @@ class BridgeGame {
    * <p>
    * 이동을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  async move() {}
+  async move(command) {}
 
   /**
    * 사용자가 게임을 다시 시도할 때 사용하는 메서드
@@ -28,6 +32,19 @@ class BridgeGame {
    * 재시작을 위해 필요한 메서드의 반환 값(return value), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
   retry() {}
+
+  #assignBridge(bridge) {
+    bridge.forEach((crossAbleSection) => {
+      if (crossAbleSection === COMMAND.UP) {
+        this.#bridge.U.push('O');
+        this.#bridge.D.push('X');
+      }
+      if (crossAbleSection === COMMAND.DOWN) {
+        this.#bridge.U.push('X');
+        this.#bridge.D.push('O');
+      }
+    });
+  }
 }
 
 export default BridgeGame;
