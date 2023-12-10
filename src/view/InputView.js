e9f3bd7f308ce2957a19a9285fd81f23770dfa41
백현영 <116708517+DOKIDOKI-b@users.noreply.github.com>
2023-12-10 13:28:09 +0900

    rename : 폴더변경

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -1,3 +1,5 @@
+import { Console } from '@woowacourse/mission-utils';
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -16,6 +18,16 @@ const InputView = {
    * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
    */
   readGameCommand() {},
+
+  async onRead(message) {
+    const userInput = await Console.readLineAsync(message);
+
+    if (userInput === undefined) {
+      throw new Error('유저 입력이 없습니다.\n');
+    }
+
+    return userInput.trim();
+  },
 };
 
 export default InputView;
