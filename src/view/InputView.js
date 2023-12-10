c649e706b7376bc8296213d2d67135c463ec3c88
BK <3031011@naver.com>
2023-12-10 14:47:21 +0900

    fix: InputView 수정
    사용자 입력 값 아래 라인에서 받도록 수정

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -1,16 +1,17 @@
+import { EOL } from 'os';
 import { Console } from '@woowacourse/mission-utils';
-import { COMMAND } from '../constant/BridgeGame';
+import { COMMAND } from '../constant/BridgeGame.js';
 
 const InputView = {
   async readBridgeSize() {
-    const bridgeSizeInput = await this.read('다리의 길이를 입력해주세요.');
+    const bridgeSizeInput = await this.read(`다리의 길이를 입력해주세요.${EOL}`);
 
     return bridgeSizeInput;
   },
 
   async readMoving() {
     const movingInput = await this.read(
-      `이동할 칸을 선택해주세요. (위: ${COMMAND.UP}, 아래: ${COMMAND.DOWN})`,
+      `이동할 칸을 선택해주세요. (위: ${COMMAND.UP}, 아래: ${COMMAND.DOWN})${EOL}`,
     );
 
     return movingInput;
@@ -18,7 +19,7 @@ const InputView = {
 
   async readGameCommand() {
     const gameCommandInput = await this.read(
-      `게임을 다시 시작할지 여부를 입력해주세요. (재시도: ${COMMAND.RESTART}, 종료: ${COMMAND.QUIT})`,
+      `게임을 다시 시작할지 여부를 입력해주세요. (재시도: ${COMMAND.RESTART}, 종료: ${COMMAND.QUIT})${EOL}`,
     );
 
     return gameCommandInput;
