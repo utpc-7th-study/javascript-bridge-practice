f27ca74579cba78d98ab4c9d23bc033c82bba9a9
백현영 <116708517+DOKIDOKI-b@users.noreply.github.com>
2023-12-10 16:55:27 +0900

    feat : user가 진행중인 다리 Map 출력기능 및 게임결과 출력 기능 추가

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -1,22 +1,32 @@
 import { Console } from '@woowacourse/mission-utils';
 
-/**
- * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
- */
 const OutputView = {
-  /**
-   * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
-   * <p>
-   * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
-  printMap() {},
+  // eslint-disable-next-line max-lines-per-function
+  printMap(moveHistory) {
+    const upside = moveHistory.map(({ position, isSuccess }) => {
+      if (position !== 'U') return ' ';
+      return isSuccess ? 'O' : 'X';
+    });
+    const downside = moveHistory.map(({ position, isSuccess }) => {
+      if (position !== 'D') return ' ';
+      return isSuccess ? 'O' : 'X';
+    });
 
-  /**
-   * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
-   * <p>
-   * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-   */
-  printResult() {},
+    this.onPrint(`[ ${upside.join(' | ')} ]`);
+    this.onPrint(`[ ${downside.join(' | ')} ]\n`);
+  },
+
+  printResult({ tryCount, isSuccess, moveHistory }) {
+    this.onPrint('\n최종 게임 결과');
+    this.printMap(moveHistory);
+
+    this.onPrint(`게임 성공 여부: ${isSuccess ? '성공' : '실패'}`);
+    this.onPrint(`총 시도한 횟수: ${tryCount}\n`);
+  },
+
+  printStartMessage() {
+    Console.print('다리 건너기 게임을 시작합니다.\n');
+  },
 
   onPrint(message) {
     Console.print(message);
