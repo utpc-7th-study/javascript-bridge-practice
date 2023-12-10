1df7a656ed24c15412d5fb9c7733d2ac619fc5e0
백현영 <116708517+DOKIDOKI-b@users.noreply.github.com>
2023-12-10 14:14:21 +0900

    feat : user가 잘못된 입력을 했을시에 재입력을 도와줄 헬퍼함수

--- src/utils/createErrorHandler.js
+++ src/utils/createErrorHandler.js
@@ -2,21 +2,46 @@ import OutputView from '../view/OutputView.js';
 
 /**
  *
- * @param {*} operator - Error 객체와 Error 객체에 대응하는 Action 객체를 가진 Map
+ * @param {Map<Error, Function>} operator - Error 객체와 Error 객체에 대응하는 Action 객체를 가진 Map
  * @returns
  */
 
 // eslint-disable-next-line max-lines-per-function
 const createErrorHandler = (operator) => {
-  return (error) => {
-    const errorAction = operator.get(error.constructor);
+  // eslint-disable-next-line max-lines-per-function
+  return async (excuteFunction) => {
+    try {
+      return await excuteFunction();
+    } catch (error) {
+      const errorAction = operator.get(error.constructor);
 
-    if (!errorAction) throw error;
+      if (!errorAction) throw error;
 
-    OutputView.printError(error.message);
+      OutputView.onPrint(error.message);
 
-    return errorAction();
+      return errorAction();
+    }
   };
 };
 
 export default createErrorHandler;
+
+//   #handleErrorAsRecursive = this.createUserInputErrorHandler();
+
+// async visitDateProcess() {
+//   return await this.#handleErrorAsRecursive(async () => {
+//     const visitDateInput = await InputView.readVisitDate();
+//     this.#discount = new Discount(visitDateInput);
+//   });
+// }
+
+//   createUserInputErrorHandler() {
+//     const operator = new Map([
+//       [VisitDateInputError, this.visitDateProcess.bind(this)],
+//       [OrderMenuInputError, this.orderProcess.bind(this)],
+//     ]);
+//     const errorHandler = createErrorHandler(operator);
+
+//     return async (excuteFunction) => {
+//       return await errorHandler(excuteFunction);
+//     };
