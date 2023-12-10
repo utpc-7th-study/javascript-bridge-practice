8656c4a0a24253daeb620a65205cc60a6acba887
BK <3031011@naver.com>
2023-12-10 13:28:35 +0900

    feat: 사용자 입력 받는 InputView 정의

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -1,21 +1,34 @@
-/**
- * 사용자로부터 입력을 받는 역할을 한다.
- */
+import { Console } from '@woowacourse/mission-utils';
+import { COMMAND } from '../constant/BridgeGame';
+
 const InputView = {
-  /**
-   * 다리의 길이를 입력받는다.
-   */
-  readBridgeSize() {},
+  readBridgeSize() {
+    const bridgeSizeInput = this.read('다리의 길이를 입력해주세요.');
 
-  /**
-   * 사용자가 이동할 칸을 입력받는다.
-   */
-  readMoving() {},
+    return bridgeSizeInput;
+  },
 
-  /**
-   * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-   */
-  readGameCommand() {},
+  readMoving() {
+    const movingInput = this.read(
+      `이동할 칸을 선택해주세요. (위: ${COMMAND.UP}, 아래: ${COMMAND.DOWN})`,
+    );
+
+    return movingInput;
+  },
+
+  readGameCommand() {
+    const gameCommandInput = this.read(
+      `게임을 다시 시작할지 여부를 입력해주세요. (재시도: ${COMMAND.RESTART}, 종료: ${COMMAND.QUIT})`,
+    );
+
+    return gameCommandInput;
+  },
+
+  read(message) {
+    const input = Console.readLineAsync(message);
+
+    return input;
+  },
 };
 
 export default InputView;
